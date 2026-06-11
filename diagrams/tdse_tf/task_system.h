dce35fb4357944fe43229ec4f1f13968a0f5b518
lautaro-fritz <lfritz@fi.uba.ar>
2026-06-11 01:09:28 -0300

    Add statechart with custom type and refactor

---
+++ diagrams/tdse_tf/task_system.h
@@ -0,0 +1,13 @@
+/*
+ * task_system.h
+ *
+ *  Created on: 11 jun 2026
+ *      Author: lauta
+ */
+
+#ifndef TASK_SYSTEM_H_
+#define TASK_SYSTEM_H_
+
+typedef enum task_system_mode {MANUAL, AUTO} task_system_mode_t;
+
+#endif /* TASK_SYSTEM_H_ */
